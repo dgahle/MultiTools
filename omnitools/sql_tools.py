f1d662cd84f91a02a0cb3150486b09358077fe96
Daljeet Singh Gahle <47973717+dgahle@users.noreply.github.com>
2022-02-03 15:49:53 +0000

    sql_tools.py module added which can be used to abbreviate building SQL queries and connecting to SQL servers

---
+++ omnitools/sql_tools.py
@@ -0,0 +1,41 @@
+from typing import Union, Optional
+from pyodbc import connect, Connection
+from pandas import read_sql_query
+
+
+user_authentication='ActiveDirectoryInteractive'
+def get_edge_sql_connection(username:str, server:str, database:str, authentication:str=user_authentication,
+                            driver:str='{ODBC Driver 17 for SQL Server}') -> Connection:
+    return connect(DRIVER=driver, Authentication=authentication,
+                   SERVER=server, DATABASE=database, UID=username)
+
+
+def build_sql_query_str(select:Union[str, list], table:str, where:Optional[Union[str, list]]=None) -> str:
+    # add SELECT data
+    select = [select] if isinstance(select, str) else select
+    sql_query = "SELECT"
+    for data in select:
+        sql_query += f" {data},"
+    sql_query = sql_query[:-1]
+    # add FROM database
+    sql_query += f" FROM {table}"
+    # add WHERE condition to sql_query
+    if where is not None:
+        where = [where] if isinstance(where, str) else where
+        sql_query += f" WHERE "
+        for where_condition in list(where):
+            sql_query += f"{where_condition} AND "
+        sql_query = sql_query[:-4]
+
+    return sql_query
+
+
+def columns_of_a_sql_table(table:str, connection) -> list:
+
+    select_sql = "TOP(0)*"
+    sql_query = build_sql_query_str(select=select_sql, table=table)
+    df = read_sql_query(sql_query, connection)
+
+    return df.columns.tolist()
+
+
