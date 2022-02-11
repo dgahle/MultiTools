67e8286163df2215ac5722442fbb39c6af5c128a
Daljeet Singh Gahle <47973717+dgahle@users.noreply.github.com>
2022-02-11 15:25:24 +0000

    Updated in preparation for PyPI release.

--- setup.py
+++ setup.py
@@ -1,17 +1,40 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='omnitools',
-    version='0.0.1',
-    packages=find_packages(include=['omnitools']),
-    license='MIT License',
-    author='Daljeet Singh Gahle',
-    description='A general repo for data processing and statistical analysis tools in Python.',
-    install_requires=[
-        'pandas',
-        'numpy',
-        'matplotlib',
-        'pyodbc',
-        'inference-tools',
-        'azure.storage.blob' ]
-    )
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Needed for dependencies
+INSTALL_REQUIRES = [    'pandas',
+                        'numpy',
+                        'matplotlib',
+                        'pyodbc',
+                        'inference-tools',
+                        'azure.storage.blob'    ]
+
+CLASSIFIERS = [ 'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+                'Intended Audience :: Developers',      # Define that your audience are developers
+                'Topic :: Software Development :: Build Tools',
+                'License :: OSI Approved :: MIT License',   # Again, pick a license
+                'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
+                'Programming Language :: Python :: 3.7' ]
+
+setup(  name='omnitools',
+        version='0.0.1',
+        url='https://github.com/dgahle/OmniTools',
+        packages=find_packages(include=['omnitools']),
+        license='MIT License',
+        author='Daljeet Singh Gahle',
+        description='A general repo for data processing and statistical analysis tools in Python.',
+        long_description_content_type='text/markdown',
+        long_description=long_description,
+        install_requires=INSTALL_REQUIRES,
+        python_requires='>=3.7',
+        zip_safe=False,
+        download_url='https://github.com/user/reponame/archive/v_01.tar.gz',  # I explain this later on
+        keywords=['data analysis', 'data processing', 'statistics', 'pandas support'],  # Keywords that define your package best
+        classifiers=CLASSIFIERS   )
+
