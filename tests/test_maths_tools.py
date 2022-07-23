0fa714643fcfb6d1620771611020572000d220c2
dgahle <dsgahle1771@outlook.com>
2022-07-23 20:51:57 +0100

    dot_convolve passed pytest when threshold set to within 5 %. Had to remove pandas_tool from omnitools inport due to issues with azure module.

--- tests/test_maths_tools.py
+++ tests/test_maths_tools.py
@@ -1,8 +1,27 @@
-from edgetools import dot_convolve
+# Import
+from omnitools import dot_convolve
+from numpy import array, ndarray
+from numpy.random import normal
 
-def test_dot_convolve():
-    data = ...
-    window_functions = ...
+
+# Variables
+
+
+# Test classes and functions
+def test_dot_convolve() -> None:
+    loc: float = 0.0
+    scale: float = 1.0
+    size: int = 100
+    data: ndarray = normal(loc, scale, size)
+    window_functions: ndarray = array([0, 1, 2, 1, 0])
     data_smoothed = dot_convolve(data, window_functions)
 
-    assert sum(data), sum(data_smoothed), "dot_convolve does not conserve area!"
+    assert abs((sum(data) - sum(data_smoothed)) / sum(data)) < 0.05, "dot_convolve does not conserve area!"
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
